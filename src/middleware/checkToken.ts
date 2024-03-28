be826d573c03c5782670e462c0ec1b499e273405
joabysonSouza <125371904+joabysonSouza@users.noreply.github.com>
2024-03-28 11:54:37 -0300

    adicinando verificação para token valido

--- src/middleware/checkToken.ts
+++ src/middleware/checkToken.ts
@@ -1,15 +1,25 @@
-import { Request, Response,  } from "express";
-import jwt, { Secret } from 'jsonwebtoken';
+import { NextFunction, Request, Response,  } from "express";
+import jwt, { Secret, VerifyErrors } from 'jsonwebtoken';
+
+
+declare global {
+    namespace Express {
+        interface Request {
+            user?: any; // Ou substitua 'any' pelo tipo apropriado do seu usuário
+        }
+    }
+}
+
 
 export const checkToken =(req:Request , res:Response ,next:()=> void)=>{
     const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(" ")[1]
-    if(!token){
+    const refresToken = authHeader && authHeader.split(" ")[1]
+    if(!refresToken){
         return res.status(401).json({message : "Acesso negado!"})
     }
     try{
         const secret = process.env.SECRET
-        jwt.verify(token, secret as Secret)
+        jwt.verify(refresToken, secret as Secret)
 
         next()
 
@@ -20,3 +30,35 @@ export const checkToken =(req:Request , res:Response ,next:()=> void)=>{
     }
 
 }
+
+
+
+
+
+
+ export const verifyRefreshToken = (req: Request, res: Response, next: NextFunction) => {
+    const {refresToken} = req.body;
+
+    const refreshSecret = process.env.REFRESH_TOKEN;
+    if (refresToken == null) {
+        return console.log("error token não definido")
+        
+     }
+
+    jwt.verify(refresToken,refreshSecret!, (err: any, user: any) => {
+        if (err) {
+            console.error(err); // Apenas para depuração
+            return res.sendStatus(403); // Retorna 403 Forbidden se houver erro na verificação do token
+        }
+        req.user = user;
+        next(); // Chama next() apenas se a verificação do token for bem-sucedida
+    });
+};
+
+
+
+
+
+
+    
+
