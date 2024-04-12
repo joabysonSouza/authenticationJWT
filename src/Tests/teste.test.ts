518a4cc992390097d3975b76735cbdae666ffa1a
joabysonSouza <125371904+joabysonSouza@users.noreply.github.com>
2024-04-12 08:59:01 -0300

    adicionando arquivo docker

--- src/Tests/teste.test.ts
+++ src/Tests/teste.test.ts
@@ -1,17 +1,34 @@
 import request from 'supertest';
-import app from '../app';
+import app from "../../src/app"
 import { User } from '../Models/User';
 import jwt from 'jsonwebtoken';
+import { checkToken } from '../middleware/checkToken';
+import { NextFunction, Request, Response } from "express";
 
-describe.only('GET /user/:id', () => {
-    it('should return status 200 and user data if user is found', async () => {
-        // Supondo que você tenha uma função `User.findById` mockada para retornar um usuário válido
-        const user = { _id: '123', name: 'John Doe', email: 'john@example.com' };
-        jest.spyOn(User, 'findById').mockResolvedValueOnce(null);
 
+jest.mock('./app', () => {
+  
+  return {
+    ...jest.requireActual('../../src/app'),
+    checkToken: (req:Request, res:Response, next:NextFunction) => {
+      // Simplesmente chama next() para passar para a próxima etapa
+      next();
+    }
+  };
+});
+
+describe('GET /user/:id', () => {
+  test('Deve retornar status 200 e o usuário se ele existir', async () => {
+    // Suponha que você tenha um usuário com ID 123 no seu banco de dados
+    const userId = '123';
     
-        const response = await request(app).get('/user/123');
-        expect(response.status).toBe(200);
-        expect(response.body).toEqual({ user });
-      });
+    // Faça a chamada para a rota
+    const response = await request(app).get(`/user/${userId}`);
+
+    // Verifique se o status da resposta é 200
+    expect(response.status).toBe(200);
+
+    // Verifique se a resposta contém o usuário
+    expect(response.body).toHaveProperty('user');
+  });
 })
